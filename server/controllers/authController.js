466d9a0159a555d191cd7f8310ad6877aa02aeaa
mysterysrishty <148486320+mysterysrishty@users.noreply.github.com>
2026-03-31 22:26:44 +0530

    fix utils path

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-const generateToken = require('../Utlis/generateToken');
+const generateToken = require('../utils/generateToken');
 
 // @desc  Register user
 // @route POST /api/auth/register
